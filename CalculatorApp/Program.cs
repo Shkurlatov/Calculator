cc5d1b37e0c1bb481fed41d55dc6f8441b3a98b0
Alexander Shkurlatov <shkurlatov80@gmail.com>
2021-10-31 13:08:13 +1200

    Test commit, after reinstall Visual Studio.

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -6,7 +6,7 @@ namespace CalculatorApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test commit, after reinstall Visual Studio.");
         }
     }
 }
